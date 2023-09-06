250b15541ad90809d8b4736a11d03e156f3014b4
LIN HTET <200393linn@gmail.com>
2023-09-06 14:37:08 +0800

    yes test

--- WheelchairAid/BusView.swift
+++ WheelchairAid/BusView.swift
@@ -5,15 +5,15 @@
 //  Created by user on 6/9/23.
 //
 
-import SwiftUI
-
-struct BusView: View {
-    var body: some View {
-        NavigationStack{
-}
-
-struct BusView_Previews: PreviewProvider {
-    static var previews: some View {
-        BusView()
-    }
-}
+//import SwiftUI
+//
+//struct BusView: View {
+//    var body: some View {
+//        NavigationStack{
+//}
+//
+//struct BusView_Previews: PreviewProvider {
+//    static var previews: some View {
+//        BusView()
+//    }
+//}
