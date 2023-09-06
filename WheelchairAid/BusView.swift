1ef4a46a47c9721daf613f8b7130d16b400787a2
LIN HTET <200393linn@gmail.com>
2023-09-06 14:27:39 +0800

    prepare final :3

---
+++ WheelchairAid/BusView.swift
@@ -0,0 +1,19 @@
+//
+//  BusView.swift
+//  WheelchairAid
+//
+//  Created by user on 6/9/23.
+//
+
+import SwiftUI
+
+struct BusView: View {
+    var body: some View {
+        
+}
+
+struct BusView_Previews: PreviewProvider {
+    static var previews: some View {
+        BusView()
+    }
+}
