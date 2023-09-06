606b45eb131129c28ddeb240bab8957b756e68ce
LIN HTET <200393linn@gmail.com>
2023-09-06 09:00:17 +0800

    hello

---
+++ WheelchairAid/bus_numberView.swift
@@ -0,0 +1,20 @@
+//
+//  bus_numberView.swift
+//  WheelchairAid
+//
+//  Created by user on 5/9/23.
+//
+
+import SwiftUI
+
+struct bus_numberView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+struct bus_numberView_Previews: PreviewProvider {
+    static var previews: some View {
+        bus_numberView()
+    }
+}
