d1f7341f53fea9d9cf577ac3cb2987a2ab01fdcd
Cyril <cjh.t01snake@gmail.com>
2023-09-07 16:43:03 +0800

    bus stop view

--- WheelchairAid/BusStopView.swift
+++ WheelchairAid/BusStopView.swift
@@ -9,9 +9,50 @@ import SwiftUI
 
 struct BusStopsView: View {
     var busNumber:String
+    private struct Bus: Identifiable {
+        let name: String
+        let number: String
+        let road: String
+        let avail: String
+        let busNumber: String
+        let color: String
+        let arriving: String
+        var id: String
+    }
+    
+    private func findByBusStop(number: String)-> [Bus]{
+        var arr:[Bus] = []
+        for i in 0...(busNumbers.count - 1){
+            if busNumbers[i] == busNumber{
+                arr.append(Bus(name: names[i], number: numbers[i], road: roads[i], avail: availability[i], busNumber: busNumbers[i], color: colors[i], arriving: arriving[i], id: String(i)))
+//                arr.append([names[i], numbers[i], roads[i], availability[i], busNumbers[i], colors[i]])
+            }
+        }
+        return arr
+    }
     var body: some View {
-        
+        let info:[Bus] = findByBusStop(number: busNumber)
         List{
+//            ForEach(info){ bus in
+//                HStack{
+//                    VStack(alignment: .leading){
+//                        Text(bus.name).padding(.vertical,5)
+//                        Text(bus.number).padding(.vertical,5)
+//                    }
+//                    Spacer()
+//                    VStack(alignment: .trailing){
+//                        Text(bus.arriving + " mins").font(.system(size: 25))
+//                        HStack{
+//                            if bus.avail == "0"{
+//                                Image(systemName: "figure.roll").foregroundColor(.red)
+//                            }else{
+//                                Image(systemName: "figure.roll").foregroundColor(.green)
+//                            }
+//                            Text(bus.avail + " available")
+//                        }
+//                    }
+//                }
+//            }
             Section(header: HStack{
                 Text("Changi Village Rd").font(.system(size: 15))
                 Spacer()
@@ -59,7 +100,7 @@ struct BusStopsView: View {
                         }
                     }
                 }
-                
+
             }
             Section(header: HStack{
                 Text("Upp Changi Rd East").font(.system(size: 15))
@@ -108,7 +149,7 @@ struct BusStopsView: View {
                         }
                     }
                 }
-                
+
             }
             Section(header: HStack{
                 Text("Bedok Rd").font(.system(size: 15))
@@ -157,13 +198,13 @@ struct BusStopsView: View {
                         }
                     }
                 }
-                
+
             }
         }
     }
                             struct BusStopsView_Previews: PreviewProvider {
                                    static var previews: some View {
-                                       BusStopsView(busNumber: "123")
+                                       BusStopsView(busNumber: "702A")
                                    }
                                }
                            }
