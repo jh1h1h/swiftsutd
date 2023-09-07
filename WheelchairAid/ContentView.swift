aa6229f4970e8fbf9d1e016e65943670f5fe5ccf
Sihan <sihan0303@gmail.com>
2023-09-07 10:36:21 +0800

    search function for both bus stop and bus done

--- WheelchairAid/ContentView.swift
+++ WheelchairAid/ContentView.swift
@@ -13,89 +13,78 @@ struct ContentView: View {
     let numbers = ["43659" , "43581", "43641" , "43589", "43649","43839","43831", "43491" , "43521" ,"43571","43579"]
     let roads = ["BT BATOK ST 34","BT BATOK WEST AVE 5","BT BATOK WEST AVE 7","BT BATOK WEST AVE 5","BT BATOK WEST AVE 7","BT BATOK WEST AVE 7","BK BATOK WEST AVE 7","BK BATOK WEST AVE 4","BT BATOK WEST AVE 5","BT BAKTOK WEST AVE 5","BT BATOK WEST AVE 5"]
     let availability = [0, 1, 2, 0, 2, 1, 0, 2, 2, 1, 1]
-    let busNumbers = ["2","12E","21A","187","98A"]
-    let colors  = ["GAS","GAS","SBS","SMRT","TTS"]
+    let busNumbers = ["2","12E","21A","187","98A","268","268B","602","603","702","702A"]
+    let colors  = ["GAS","GAS","SBS","SMRT","TTS","SBS","SBS","GAS","TTS","SMRT","SMRT"]
     
     var body: some View {
-            NavigationStack {
-                NavigationLink("Bus Stop"){
-                    List {
-                        ForEach(searchResults, id: \.self) { name in
-                            NavigationLink {
-                                Text(String(name))
-                            } label: {
-                                let index = numbers.firstIndex(of: name)
-                                VStack(alignment: .leading){
-                                    Text(name)
-                                        .font(.system(size: 15))
+        NavigationStack {
+            NavigationLink("Bus Stop"){
+                List {
+                    ForEach(searchResultsBus, id: \.self) { name in
+                        NavigationLink {
+                            bus_numberView(busstop: name)
+                        } label: {
+                            let index = numbers.firstIndex(of: name)
+                            VStack(alignment: .leading){
+                                Text(name)
+                                    .font(.system(size: 15))
+                                    .foregroundColor(/*@START_MENU_TOKEN@*/Color(hue: 0.098, saturation: 0.994, brightness: 0.586)/*@END_MENU_TOKEN@*/)
+                                Text(names[index ?? 0]).bold()
+                                HStack{
+                                    Image(systemName: "circle.fill")
+                                        .font(.system(size: 5))
                                         .foregroundColor(/*@START_MENU_TOKEN@*/Color(hue: 0.098, saturation: 0.994, brightness: 0.586)/*@END_MENU_TOKEN@*/)
-                                    Text(names[index ?? 0]).bold()
-                                    HStack{
-                                        Image(systemName: "circle.fill")
-                                            .font(.system(size: 5))
-                                            .foregroundColor(/*@START_MENU_TOKEN@*/Color(hue: 0.098, saturation: 0.994, brightness: 0.586)/*@END_MENU_TOKEN@*/)
-                                        Text(roads[index ?? 0])
-                                            .font(.system(size: 10))
-                                    }
-                                    
+                                    Text(roads[index ?? 0])
+                                        .font(.system(size: 10))
                                 }
+                                
                             }
                         }
                     }
-                    .navigationTitle("Search for bus stop")
-                    .searchable(text: $searchText)
                 }
-                NavigationLink("Bus Number"){
-                    List {
-                        ForEach(searchResults, id: \.self) { name in
-                            NavigationLink {
-                                Text(String(name))
-                            } label: {
-                                let index = numbers.firstIndex(of: name)
-                                VStack(alignment: .leading){
-                                    Text(name)
-                                        .font(.system(size: 15))
-                                        .foregroundColor(/*@START_MENU_TOKEN@*/Color(hue: 0.098, saturation: 0.994, brightness: 0.586)/*@END_MENU_TOKEN@*/)
-                                    Text(names[index ?? 0]).bold()
-                                    HStack{
-                                        Image(systemName: "circle.fill")
-                                            .font(.system(size: 5))
-                                            .foregroundColor(/*@START_MENU_TOKEN@*/Color(hue: 0.098, saturation: 0.994, brightness: 0.586)/*@END_MENU_TOKEN@*/)
-                                        Text(roads[index ?? 0])
-                                            .font(.system(size: 10))
-                                    }
-                                    
-                                }
-                            }
-                        }
-                    }
-                    .navigationTitle("Search for bus stop")
-                    .searchable(text: $searchText)
-                }
+                .navigationTitle("Search for bus stop")
+                .searchable(text: $searchText)
             }
-//            TabView {
-//                HomeView()
-//                    .tabItem {
-//                        Label("Received", systemImage: "tray.and.arrow.down.fill")
-//                    }
-//                ButtonView(names: names, numbers: numbers, roads: roads)
-//                    .tabItem {
-//                        Label("Received", systemImage: "tray.and.arrow.down.fill")
-//                    }
-//                ProfileView()
-//                    .tabItem {
-//                        Label("Sent", systemImage: "tray.and.arrow.up.fill")
-//                    }
-//            }
-
-//        VStack{
-//            Image("bus")
-//        }
-    }
-    var searchResults:[String]{
-        return numbers.filter{$0.contains(searchText)}
+            NavigationLink("Bus Number"){
+                List{
+                    ForEach(searchResultsBusStop, id: \.self) { name in
+                        NavigationLink {
+                            BusStopsView(busNumber: name)
+                        } label: {
+                            HStack{
+                                let index = busNumbers.firstIndex(of: name)
+                                Text(name)
+                                Spacer()
+                                if(colors[index ?? 0]=="GAS"){
+                                    Text(colors[index ?? 0]).foregroundColor(.yellow)
+                                }
+                                else if(colors[index ?? 0]=="TTS"){
+                                    Text(colors[index ?? 0]).foregroundColor(.green)
+                                }
+                                else if(colors[index ?? 0]=="SMRT"){
+                                    Text(colors[index ?? 0]).foregroundColor(.red)
+                                }
+                                else{
+                                    Text(colors[index ?? 0]).foregroundColor(.purple)
+                                }
+                            }
+                        }
+                        
+                    }
+                }
+                .navigationTitle("Search for Bus Numbers")
+                .searchable(text: $searchText)
+        }
     }
 }
+    var searchResultsBus:[String]{
+        return numbers.filter{$0.contains(searchText)}
+    }
+    var searchResultsBusStop:[String]{
+        return busNumbers.filter{$0.contains(searchText)}
+    }
+}
+
 
 struct ContentView_Previews: PreviewProvider {
     static var previews: some View {
